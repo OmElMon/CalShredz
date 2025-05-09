c02be7f9fba512163feee20e35f7bb6b6134a0ab
Omar Elharbili <135235599+OmElMon@users.noreply.github.com>
2025-05-09 10:55:50 -0400

    Update

--- Postcss.config.js
+++ Postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: './tailwind.config.ts', // explicitly point to your TS config
     },
-  }
+    autoprefixer: {},
+  },
+};
